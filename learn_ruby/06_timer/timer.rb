012bbaf6798002480c289e792799571617ab7cfd
Tracy Sedgwick <tracyjs@gmail.com>
2018-02-01 16:56:27 -0800

    Completer exercise 06

---
+++ learn_ruby/06_timer/timer.rb
@@ -0,0 +1,39 @@
+class Timer
+	attr_accessor :seconds
+	attr_writer :num
+
+	@@divisors = [3600, 60, 1]
+	
+	def initialize(seconds = 0)
+		@seconds = seconds
+	end
+
+	def time_string
+		time_arr = [0, 0, 0]
+		@@divisors.each_with_index do |divisor, index|
+			while @seconds/divisor >= 1
+				time_arr[index] += 1
+				@seconds = @seconds - divisor
+			end
+			# @time_arr[index] = padded(@time_arr[index])
+			padded(time_arr[index])
+			time_arr[index] = @num
+		end			
+		time_arr.join(":")
+	end
+
+	def padded(num) 
+		if num < 10
+			@num = "0" + num.to_s
+		else
+			@num = num.to_s
+		end
+	end
+end
+
+# timer = Timer.new
+# timer.seconds = 6397
+# puts timer.time_string
+# puts timer.padded(11)
+
+
